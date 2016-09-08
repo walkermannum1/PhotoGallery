fbb7bd372150ef8210b5281b1d8a2cc999280e3c
walkermannum1 <2469377884@qq.com>
2016-09-08 17:12:56 +0800

    Weibo editon

--- app/src/main/java/com/example/user/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/user/photogallery/PhotoGalleryFragment.java
@@ -1,9 +1,14 @@
 package com.example.user.photogallery;
 
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.io.IOException;
 
@@ -19,12 +24,28 @@ public class PhotoGalleryFragment extends Fragment {
         return new PhotoGalleryFragment();
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        new FetchItemsTask().execute();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_photo_gallery_recycler_view);
+
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        return v;
+    }
+
     private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... params) {
             try {
-                String result = new PhotoFetcher().getUrlString("https://www.bignerdranch.com");
+                String result = new WeiboFetcher().getUrlString("https://www.bignerdranch.com");
                 Log.i(TAG, "Fetched contents of URL: " + result);
             } catch (IOException ioe) {
                 Log.e(TAG, "Failed to fetch URL: ", ioe);
--- app/src/main/java/com/example/user/photogallery/WeiboFetcher.java
+++ app/src/main/java/com/example/user/photogallery/WeiboFetcher.java
@@ -1,5 +1,8 @@
 package com.example.user.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,7 +12,11 @@ import java.net.URL;
 /**
  * Created by user on 2016/9/8.
  */
-public class PhotoFetcher {
+public class WeiboFetcher {
+
+    private static final String TAG = "WeiboFetcher";
+    private static final String API_KEY = "2133803110";
+
     public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
@@ -24,7 +31,7 @@ public class PhotoFetcher {
 
             int bytesRead = 0;
             byte[] buffer = new byte[1024];
-            while ((bytesRead = in.read(buffer)) >0 ) {
+            while ((bytesRead = in.read(buffer)) > 0 ) {
                 out.write(buffer, 0, bytesRead);
             }
                 out.close();
@@ -36,6 +43,18 @@ public class PhotoFetcher {
 
     public String getUrlString(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
+    }
+
+    public void fetchItems() {
+        try {
+            String url = Uri.parse("http://open.weibo.com/services/rest/").buildUpon().appendQueryParameter("method", "weibo.photos.getPeople")
+                    .appendQueryParameter("api_key",API_KEY).appendQueryParameter("format", "json").appendQueryParameter("nojsoncallback", "1")
+                    .appendQueryParameter("extras", "url_s").build().toString();
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+        } catch (IOException ioe) {
+            Log.e(TAG, "Failed to fetch items", ioe);
+        }
     }
 }
 
