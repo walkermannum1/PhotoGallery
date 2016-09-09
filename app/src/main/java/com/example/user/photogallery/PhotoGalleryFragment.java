aca0b3afeafd64d235b8141fe0ee451c9481566e
walkermannum1 <2469377884@qq.com>
2016-09-09 17:58:49 +0800

    Show picture.

--- app/src/main/java/com/example/user/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/user/photogallery/PhotoGalleryFragment.java
@@ -5,13 +5,12 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.List;
 
