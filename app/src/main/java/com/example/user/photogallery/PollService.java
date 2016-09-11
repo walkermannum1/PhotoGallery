2108072678f68ba7674a9a16b8070113ba3c3240
walkermannu1 <2469377884@qq.com>
2016-09-11 22:16:52 +0800

    Alarm

--- app/src/main/java/com/example/user/photogallery/PollService.java
+++ app/src/main/java/com/example/user/photogallery/PollService.java
@@ -1,10 +1,15 @@
 package com.example.user.photogallery;
 
+import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.os.SystemClock;
 import android.util.Log;
+
+import java.util.List;
 
 /**
  * Created by gaung on 2016/9/11.
@@ -12,9 +17,30 @@ import android.util.Log;
 public class PollService extends IntentService {
 
     private static final String TAG = "PollService";
+    private static final int POLL_INTERVAL = 1000 * 60; //60 seconds
 
     public static Intent newIntent(Context context){
         return new Intent(context, PollService.class);
+    }
+
+    public static void setServiceAlarm(Context context, boolean isOn) {
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, 0);
+
+        AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+
+        if (isOn) {
+            alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime(), POLL_INTERVAL, pi);
+        } else {
+            alarmManager.cancel(pi);
+            pi.cancel();
+        }
+    }
+
+    public static boolean isServiceAlarmOn(Context context) {
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_NO_CREATE);
+        return pi != null;
     }
 
     public PollService() {
@@ -26,7 +52,28 @@ public class PollService extends IntentService {
         if (!isNetworkAvailableAndConnected()) {
             return;
         }
-        Log.i(TAG, "Received an intent: " + intent);
+        String query = QueryPreferences.getStoredQuery(this);
+        String lastResultId = QueryPreferences.getLastResultId(this);
+        List<GalleryItem> items;
+
+        if (query == null) {
+            items = new PhotoFetcher().fetchRecentPhotos();
+        } else {
+            items = new PhotoFetcher().searchPhotos(query);
+        }
+
+        if (items.size() ==0) {
+            return;
+        }
+
+        String resultId = items.get(0).getId();
+        if (resultId.equals(lastResultId)) {
+            Log.i(TAG, "Got an old result: " + resultId);
+        } else {
+            Log.i(TAG, "Got a new result: " + resultId);
+        }
+
+        QueryPreferences.setLastResultId(this, resultId);
     }
 
     private boolean isNetworkAvailableAndConnected() {
